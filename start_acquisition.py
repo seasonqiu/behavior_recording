fabf965100cf9f490fb748d3dfd4d6ba4b18cf21
Season Qiu <season.qiu@yahoo.com>
2024-07-17 13:47:47 -0400

    Update start_acquisition.py

--- start_acquisition.py
+++ start_acquisition.py
@@ -20,7 +20,7 @@ with open(save_path,mode='w',newline='') as file:
 while True:
     try:
         ser_bytes = ser.readline()
-        decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode('UTF-16')#'UTF-8')
+        decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode('utf-8')#'UTF-8')
         print(decoded_bytes)
         current_event = decoded_bytes.split(",")
         with open(save_path,"a") as f:
