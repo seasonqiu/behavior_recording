0963a9b832d7799ae30940860c76edf222498da5
Season Qiu <season.qiu@yahoo.com>
2025-08-04 15:22:00 -0400

    Create generate_img.py

---
+++ generate_img.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+# Replace with your screen resolution
+width, height = 1920, 1080 # in bash: xdpyinfo | grep dimensions to acquire the dimension of the screen
+
+# Create all-white image (255, 255, 255 is white)
+white_image = Image.new("RGB", (width, height), color=(255, 255, 255))
+
+# Save as JPEG
+white_image.save("white_fullscreen.jpg", "JPEG")
